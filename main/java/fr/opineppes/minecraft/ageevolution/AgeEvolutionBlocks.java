fe4768d3a3bb173f95264b3480f0bb05873d54c0
Opineppes <quentin.legrand.627@gmail.com>
2019-06-27 13:07:17 +0200

    Ajout des proprieté de blocs, des loots des blocs et du model de l'extincteur

--- main/java/fr/opineppes/minecraft/ageevolution/AgeEvolutionBlocks.java
+++ main/java/fr/opineppes/minecraft/ageevolution/AgeEvolutionBlocks.java
@@ -10,14 +10,14 @@ import net.minecraft.block.Material;
 
 public class AgeEvolutionBlocks {
 	//Cube
-	public static final Block COLORED_COBBLESTONE = new Block(FabricBlockSettings.of(Material.STONE).build());
-	public static final Block FLOOR_TILES = new Block(FabricBlockSettings.of(Material.STONE).build());
-	public static final Block FLOOR_TILE_DARK = new Block(FabricBlockSettings.of(Material.STONE).build());
-	public static final Block FLOOR_TILE_LIGHT = new Block(FabricBlockSettings.of(Material.STONE).build());
+	public static final Block COLORED_COBBLESTONE = new Block(FabricBlockSettings.of(Material.STONE).strength(1.5F, 6.0F).build());
+	public static final Block FLOOR_TILES = new Block(FabricBlockSettings.of(Material.STONE).strength(1.5F, 6.0F).build());
+	public static final Block FLOOR_TILE_DARK = new Block(FabricBlockSettings.of(Material.STONE).strength(1.5F, 6.0F).build());
+	public static final Block FLOOR_TILE_LIGHT = new Block(FabricBlockSettings.of(Material.STONE).strength(1.5F, 6.0F).build());
 	
 	//Avec model perso
-	public static final Block SCHOOL_CHAIR = new SchoolChair(FabricBlockSettings.of(Material.METAL).build());
-	public static final Block SCHOOL_TABLE = new SchoolTable(FabricBlockSettings.of(Material.METAL).build());
-	public static final Block CEILING_LIGHT = new CeilingLight(FabricBlockSettings.of(Material.METAL).noCollision().lightLevel(15).build());
-	public static final Block CEILING_LIGHT_BARS = new CeilingLightBars(FabricBlockSettings.of(Material.METAL).noCollision().lightLevel(15).build());
+	public static final Block SCHOOL_CHAIR = new SchoolChair(FabricBlockSettings.of(Material.METAL).breakByHand(true).strength(2.0F, 3.0F).build());
+	public static final Block SCHOOL_TABLE = new SchoolTable(FabricBlockSettings.of(Material.METAL).breakByHand(true).strength(2.0F, 3.0F).build());
+	public static final Block CEILING_LIGHT = new CeilingLight(FabricBlockSettings.of(Material.METAL).breakByHand(true).strength(2.0F, 3.0F).noCollision().lightLevel(15).build());
+	public static final Block CEILING_LIGHT_BARS = new CeilingLightBars(FabricBlockSettings.of(Material.METAL).breakByHand(true).strength(2.0F, 3.0F).noCollision().lightLevel(15).build());
 }
