91f371a9a6732567c332257a479e1882d979a2fc
Opineppes <quentin.legrand.627@gmail.com>
2019-06-28 00:33:05 +0200

    Reorganisation des enregistrement de bloc et d'items

--- main/java/fr/opineppes/minecraft/ageevolution/AgeEvolution.java
+++ main/java/fr/opineppes/minecraft/ageevolution/AgeEvolution.java
@@ -1,48 +1,15 @@
 package fr.opineppes.minecraft.ageevolution;
 
-import fr.opineppes.minecraft.ageevolution.events.WorldTickEventListener;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
-import net.fabricmc.fabric.api.event.world.WorldTickCallback;
-import net.minecraft.item.BlockItem;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
 
 public class AgeEvolution implements ModInitializer {
-	
-	public static final ItemGroup GROUP_GUNS = FabricItemGroupBuilder.build(new Identifier("ageevolution", "guns"), () -> new ItemStack(AgeEvolutionItems.AWP_GUN));
-	public static final ItemGroup GROUP_DECO = FabricItemGroupBuilder.build(new Identifier("ageevolution", "guns"), () -> new ItemStack(AgeEvolutionBlocks.COLORED_COBBLESTONE));
-	
+
 	@Override
 	public void onInitialize() {
-		Registry.register(Registry.BLOCK, new Identifier("ageevolution", "colored_cobblestone"), AgeEvolutionBlocks.COLORED_COBBLESTONE);
-		Registry.register(Registry.BLOCK, new Identifier("ageevolution", "floor_tiles"), AgeEvolutionBlocks.FLOOR_TILES);
-		Registry.register(Registry.BLOCK, new Identifier("ageevolution", "floor_tile_dark"), AgeEvolutionBlocks.FLOOR_TILE_DARK);
-		Registry.register(Registry.BLOCK, new Identifier("ageevolution", "floor_tile_light"), AgeEvolutionBlocks.FLOOR_TILE_LIGHT);
-		Registry.register(Registry.BLOCK, new Identifier("ageevolution", "school_chair"), AgeEvolutionBlocks.SCHOOL_CHAIR);
-		Registry.register(Registry.BLOCK, new Identifier("ageevolution", "school_table"), AgeEvolutionBlocks.SCHOOL_TABLE);
-		Registry.register(Registry.BLOCK, new Identifier("ageevolution", "ceiling_light"), AgeEvolutionBlocks.CEILING_LIGHT);
-		Registry.register(Registry.BLOCK, new Identifier("ageevolution", "ceiling_light_bars"), AgeEvolutionBlocks.CEILING_LIGHT_BARS);
-		Registry.register(Registry.BLOCK, new Identifier("ageevolution", "cracked_floor_tiles"), AgeEvolutionBlocks.CRACKED_FLOOR_TILES);
-		Registry.register(Registry.BLOCK, new Identifier("ageevolution", "fire_extinguisher"), AgeEvolutionBlocks.FIRE_EXTINGUISHER);
 		
-		Registry.register(Registry.ITEM, new Identifier("ageevolution", "magic_wand"), AgeEvolutionItems.MAGIC_WAND);
-		Registry.register(Registry.ITEM, new Identifier("ageevolution", "awp_gun"), AgeEvolutionItems.AWP_GUN);
-		Registry.register(Registry.ITEM, new Identifier("ageevolution", "fn_scar_gun"), AgeEvolutionItems.FN_SCAR_GUN);
-		
-		Registry.register(Registry.ITEM, new Identifier("ageevolution", "colored_cobblestone"), new BlockItem(AgeEvolutionBlocks.COLORED_COBBLESTONE, new Item.Settings().group(GROUP_DECO)));
-		Registry.register(Registry.ITEM, new Identifier("ageevolution", "floor_tiles"), new BlockItem(AgeEvolutionBlocks.FLOOR_TILES, new Item.Settings().group(GROUP_DECO)));
-		Registry.register(Registry.ITEM, new Identifier("ageevolution", "floor_tile_dark"), new BlockItem(AgeEvolutionBlocks.FLOOR_TILE_DARK, new Item.Settings().group(GROUP_DECO)));
-		Registry.register(Registry.ITEM, new Identifier("ageevolution", "floor_tile_light"), new BlockItem(AgeEvolutionBlocks.FLOOR_TILE_LIGHT, new Item.Settings().group(GROUP_DECO)));
-		Registry.register(Registry.ITEM, new Identifier("ageevolution", "school_chair"), new BlockItem(AgeEvolutionBlocks.SCHOOL_CHAIR, new Item.Settings().group(GROUP_DECO)));
-		Registry.register(Registry.ITEM, new Identifier("ageevolution", "school_table"), new BlockItem(AgeEvolutionBlocks.SCHOOL_TABLE, new Item.Settings().group(GROUP_DECO)));
-		Registry.register(Registry.ITEM, new Identifier("ageevolution", "ceiling_light"), new BlockItem(AgeEvolutionBlocks.CEILING_LIGHT, new Item.Settings().group(GROUP_DECO)));
-		Registry.register(Registry.ITEM, new Identifier("ageevolution", "ceiling_light_bars"), new BlockItem(AgeEvolutionBlocks.CEILING_LIGHT_BARS, new Item.Settings().group(GROUP_DECO)));
-		Registry.register(Registry.ITEM, new Identifier("ageevolution", "cracked_floor_tiles"), new BlockItem(AgeEvolutionBlocks.CRACKED_FLOOR_TILES, new Item.Settings().group(GROUP_DECO)));
-		Registry.register(Registry.ITEM, new Identifier("ageevolution", "fire_extinguisher"), new BlockItem(AgeEvolutionBlocks.FIRE_EXTINGUISHER, new Item.Settings().group(GROUP_DECO)));
+		//Deplacement des enregistrement dans les fichier respectif
+		AgeEvolutionBlocks.registerAllBlocks();
+		AgeEvolutionItems.registerAllItems();
 		
 		//Registry.register(Registry.ENTITY_TYPE, new Identifier("ageevolution", "chair"), AgeEvolutionEntities.CHAIR);
 		
--- main/java/fr/opineppes/minecraft/ageevolution/AgeEvolutionItems.java
+++ main/java/fr/opineppes/minecraft/ageevolution/AgeEvolutionItems.java
@@ -3,10 +3,46 @@ package fr.opineppes.minecraft.ageevolution;
 import fr.opineppes.minecraft.ageevolution.items.AWPGun;
 import fr.opineppes.minecraft.ageevolution.items.MagicWand;
 import fr.opineppes.minecraft.ageevolution.items.FnScarGun;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
 public class AgeEvolutionItems {
-	public static final Item MAGIC_WAND = new MagicWand(new Item.Settings().group(AgeEvolution.GROUP_GUNS));
-	public static final Item AWP_GUN = new AWPGun(new Item.Settings().group(AgeEvolution.GROUP_GUNS));
-	public static final Item FN_SCAR_GUN = new FnScarGun(new Item.Settings().group(AgeEvolution.GROUP_GUNS));
+	//Items
+	public static final Item MAGIC_WAND = new MagicWand(new Item.Settings().group(AgeEvolutionItemGroups.GROUP_GUNS));
+	public static final Item AWP_GUN = new AWPGun(new Item.Settings().group(AgeEvolutionItemGroups.GROUP_GUNS));
+	public static final Item FN_SCAR_GUN = new FnScarGun(new Item.Settings().group(AgeEvolutionItemGroups.GROUP_GUNS));
+	
+	//ItemsBlocks
+	public static final Item COLORED_COBBLESTONE = new BlockItem(AgeEvolutionBlocks.COLORED_COBBLESTONE, new Item.Settings().group(AgeEvolutionItemGroups.GROUP_DECO));
+	public static final Item FLOOR_TILES = new BlockItem(AgeEvolutionBlocks.FLOOR_TILES, new Item.Settings().group(AgeEvolutionItemGroups.GROUP_DECO));
+	public static final Item FLOOR_TILE_DARK = new BlockItem(AgeEvolutionBlocks.FLOOR_TILE_DARK, new Item.Settings().group(AgeEvolutionItemGroups.GROUP_DECO));
+	public static final Item FLOOR_TILE_LIGHT = new BlockItem(AgeEvolutionBlocks.FLOOR_TILE_LIGHT, new Item.Settings().group(AgeEvolutionItemGroups.GROUP_DECO));
+	public static final Item SCHOOL_CHAIR = new BlockItem(AgeEvolutionBlocks.SCHOOL_CHAIR, new Item.Settings().group(AgeEvolutionItemGroups.GROUP_DECO));
+	public static final Item SCHOOL_TABLE = new BlockItem(AgeEvolutionBlocks.SCHOOL_TABLE, new Item.Settings().group(AgeEvolutionItemGroups.GROUP_DECO));
+	public static final Item CEILING_LIGHT = new BlockItem(AgeEvolutionBlocks.CEILING_LIGHT, new Item.Settings().group(AgeEvolutionItemGroups.GROUP_DECO));
+	public static final Item CEILING_LIGHT_BARS = new BlockItem(AgeEvolutionBlocks.CEILING_LIGHT_BARS, new Item.Settings().group(AgeEvolutionItemGroups.GROUP_DECO));
+	public static final Item CRACKED_FLOOR_TILES = new BlockItem(AgeEvolutionBlocks.CRACKED_FLOOR_TILES, new Item.Settings().group(AgeEvolutionItemGroups.GROUP_DECO));
+	public static final Item FIRE_EXTINGUISHER = new BlockItem(AgeEvolutionBlocks.FIRE_EXTINGUISHER, new Item.Settings().group(AgeEvolutionItemGroups.GROUP_DECO));
+	
+	
+	public static void registerAllItems()
+	{
+		Registry.register(Registry.ITEM, new Identifier("ageevolution", "magic_wand"), MAGIC_WAND);
+		Registry.register(Registry.ITEM, new Identifier("ageevolution", "awp_gun"), AWP_GUN);
+		Registry.register(Registry.ITEM, new Identifier("ageevolution", "fn_scar_gun"), FN_SCAR_GUN);
+		
+		Registry.register(Registry.ITEM, new Identifier("ageevolution", "colored_cobblestone"), COLORED_COBBLESTONE);
+		Registry.register(Registry.ITEM, new Identifier("ageevolution", "floor_tiles"), FLOOR_TILES);
+		Registry.register(Registry.ITEM, new Identifier("ageevolution", "floor_tile_dark"), FLOOR_TILE_DARK);
+		Registry.register(Registry.ITEM, new Identifier("ageevolution", "floor_tile_light"), FLOOR_TILE_LIGHT);
+		Registry.register(Registry.ITEM, new Identifier("ageevolution", "school_chair"), SCHOOL_CHAIR);
+		Registry.register(Registry.ITEM, new Identifier("ageevolution", "school_table"), SCHOOL_TABLE);
+		Registry.register(Registry.ITEM, new Identifier("ageevolution", "ceiling_light"), CEILING_LIGHT);
+		Registry.register(Registry.ITEM, new Identifier("ageevolution", "ceiling_light_bars"), CEILING_LIGHT_BARS);
+		Registry.register(Registry.ITEM, new Identifier("ageevolution", "cracked_floor_tiles"), CRACKED_FLOOR_TILES);
+		Registry.register(Registry.ITEM, new Identifier("ageevolution", "fire_extinguisher"), FIRE_EXTINGUISHER);
+		
+	}
 }
